8fff4d85d281208b24b08cfdd035a65d7ef53b60
Kieran Wardle <kieran.wardle@ons.gov.uk>
2017-03-06 13:54:44 +0000

    CTPA-1143 fixed error and rubocopped

--- eqFlushScript.rb
+++ eqFlushScript.rb
@@ -4,50 +4,49 @@ require 'json'
 require 'ons-jwe'
 require 'openssl'
 
-KEY_ID                    = 'EDCRRM'.freeze
+KEY_ID = 'EDCRRM'.freeze
 config_file = YAML.load_file(File.join(__dir__, '/config.yml'))
-public_key = config_file['eq-service']['public_key']
-private_key = config_file['eq-service']['private_key']
-private_key_passphrase =  config_file['eq-service']['private_key_passphrase']
+public_key_file = config_file['eq-service']['public_key']
+private_key_file = config_file['eq-service']['private_key']
+private_key_passphrase_file = config_file['eq-service']['private_key_passphrase']
 
 def load_key_from_file(file, passphrase = nil)
-  OpenSSL::PKey::RSA.new(File.read(File.join(__dir__, file)), passphrase)
+    OpenSSL::PKey::RSA.new(File.read(File.join(__dir__, file)), passphrase)
 end
 
 port = 4567
-eqServer = ARGV[0]
+eq_server = ARGV[0]
 
-fileObj = File.new('testData', "r")
+file_obj = File.new('testData', 'r')
 
-  while (line = fileObj.gets)
-    claims = {
-      line: line
-    }
-    public_key  = load_key_from_file(public_key)
-    private_key = load_key_from_file(private_key,
-                                    private_key_passphrase)
-    token  = JWEToken.new(KEY_ID, claims, public_key, private_key)
+while (line = file_obj.gets)
+  claims = {
+    line: line
+  }
+  public_key  = load_key_from_file(public_key_file)
+  private_key = load_key_from_file(private_key_file,
+                                   private_key_passphrase_file)
+  token = JWEToken.new(KEY_ID, claims, public_key, private_key)
 
-    RestClient.post("#{eqServer}:#{port}/flush/?token=#{token.value}", {
-      #line: line,
-    }) do |post_response, _request, _result, &_block|
-          if post_response.code == 200
-              puts '200 Flush successful.'
-          end
-          if post_response.code == 401
-              puts '401 Error: Authentication Failure'
-          end
-          if post_response.code == 403
-              puts '403 Error: Permission Denied'
-          end
-          if post_response.code == 404
-              puts '404 Error: Survey Response not found for case ' + line
-          end
-          if post_response.code == 500
-              puts '500 Error: Flushing failed'
-          end
-       end
-    sleep(3)
+  RestClient.post("#{eq_server}:#{port}/flush?token=#{token.value}", {
+                  }) do |post_response, _request, _result, &_block|
+    if post_response.code == 200
+      puts '200 Flush successful.'
+    end
+    if post_response.code == 401
+      puts '401 Error: Authentication Failure'
+    end
+    if post_response.code == 403
+      puts '403 Error: Permission Denied'
+    end
+    if post_response.code == 404
+      puts '404 Error: Survey Response not found for case ' + line
+    end
+    if post_response.code == 500
+      puts '500 Error: Flushing failed'
+    end
   end
+  sleep(3)
+end
 
-fileObj.close
+file_obj.close
--- test.rb
+++ test.rb
@@ -6,12 +6,7 @@ require 'sinatra'
 require 'jwt'
 require 'yaml'
 
-post '/flush/' do
+post '/flush' do
   token = params[:token]
   puts token
-  config_file = YAML.load_file(File.join(__dir__, '/config.yml'))
-  public_key = config_file['eq-service']['public_key']
-  payload = token.value[0, token.value.index('.')]
-  #payload = JWT.decode token, public_key, true, { :algorithm => 'RS256' }
-  puts(payload)
 end
