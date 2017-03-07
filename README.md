4d2b3c9557093fc62ace0bc6e74581dcdca2f89e
Kieran Wardle <kieran.wardle@ons.gov.uk>
2017-03-07 10:58:47 +0000

    CTPA-1143 Code review changes, added Gemfile and README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rm-response-flusher
 Script to flush partially completed questionnaire responses through eQ.
+
+Usage: run with './eq_flush_script.rb <file_name> <eq_server>'
--- eqFlushScript.rb
+++ eqFlushScript.rb
@@ -1,49 +1,58 @@
+#!/usr/bin/env ruby
 require 'rest_client'
 require 'yaml'
 require 'json'
 require 'ons-jwe'
 require 'openssl'
 
+unless ARGV.length == 2
+  puts 'Usage: eq_flush_script.rb <input.csv> <server>'
+  exit
+end
+
 KEY_ID = 'EDCRRM'.freeze
 config_file = YAML.load_file(File.join(__dir__, '/config.yml'))
 public_key_file = config_file['eq-service']['public_key']
 private_key_file = config_file['eq-service']['private_key']
-private_key_passphrase_file = config_file['eq-service']['private_key_passphrase']
+private_key_pass_file = config_file['eq-service']['private_key_passphrase']
+port = config_file['eq-service']['port']
 
 def load_key_from_file(file, passphrase = nil)
-    OpenSSL::PKey::RSA.new(File.read(File.join(__dir__, file)), passphrase)
+  OpenSSL::PKey::RSA.new(File.read(File.join(__dir__, file)), passphrase)
 end
 
-port = 4567
-eq_server = ARGV[0]
+eq_server = ARGV[1]
+input_file = ARGV[0]
 
-file_obj = File.new('testData', 'r')
+file_obj = File.new(input_file, 'r')
+
+public_key  = load_key_from_file(public_key_file)
+private_key = load_key_from_file(private_key_file,
+                                 private_key_pass_file)
 
 while (line = file_obj.gets)
+
   claims = {
-    line: line
+    collection_exercise_sid: '0',
+    eq_id: 'census',
+    exp: Time.now.to_i + 60 * 60,
+    iat: Time.now.to_i,
+    roles: ['flusher'],
+    ru_ref: line,
+    tx_id: SecureRandom.uuid
   }
-  public_key  = load_key_from_file(public_key_file)
-  private_key = load_key_from_file(private_key_file,
-                                   private_key_passphrase_file)
+
   token = JWEToken.new(KEY_ID, claims, public_key, private_key)
 
   RestClient.post("#{eq_server}:#{port}/flush?token=#{token.value}", {
                   }) do |post_response, _request, _result, &_block|
-    if post_response.code == 200
-      puts '200 Flush successful.'
-    end
-    if post_response.code == 401
-      puts '401 Error: Authentication Failure'
-    end
-    if post_response.code == 403
-      puts '403 Error: Permission Denied'
-    end
-    if post_response.code == 404
-      puts '404 Error: Survey Response not found for case ' + line
-    end
-    if post_response.code == 500
-      puts '500 Error: Flushing failed'
+    code = post_response.code
+    case code
+    when 200 then $stdout.puts '200 Flush successful.'
+    when 401 then $stderr.puts '401 Error: Authentication Failure'
+    when 403 then $stderr.puts '403 Error: Permission Denied'
+    when 404 then $stderr.puts '404 Error: Survey Response not found for case ' + line
+    when 500 then $stderr.puts '500 Error: Flushing failed'
     end
   end
   sleep(3)
